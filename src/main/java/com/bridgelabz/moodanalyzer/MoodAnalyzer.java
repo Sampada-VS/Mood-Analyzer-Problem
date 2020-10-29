c3447e87838ded37fdfd6ad7b951b14b8e8c422f
Sampada-VS <samssvv1@gmail.com>
2020-10-30 02:18:10 +0530

    Refactoring- to make analyse method with no parameters

--- src/main/java/com/bridgelabz/moodanalyzer/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/moodanalyzer/MoodAnalyzer.java
@@ -1,8 +1,14 @@
 package com.bridgelabz.moodanalyzer;
 
 public class MoodAnalyzer {
+	
+	private String message;
 
-	public String analyseMood (String message) {
+	public MoodAnalyzer(String message) {
+		this.message=message;
+	}
+
+	public String analyseMood () {
 		if (message.contains("Sad"))
 			return "SAD";
 		else
--- src/test/java/com/bridgelabz/moodanalyzer/TestMoodAnalyzer.java
+++ src/test/java/com/bridgelabz/moodanalyzer/TestMoodAnalyzer.java
@@ -1,32 +1,19 @@
 package com.bridgelabz.moodanalyzer;
 
 import static org.junit.Assert.*;
-
-import org.junit.AfterClass;
-import org.junit.Assert;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class TestMoodAnalyzer {
-	static  MoodAnalyzer moodanalyze;
-
-	@BeforeClass
-	public static void createObj() {
-		moodanalyze = new MoodAnalyzer();
-	}
-
-	@AfterClass
-	public static void nullObj() {
-		moodanalyze = null;
-	}
 	
 	@Test
 	public void givenMessage_whenInSadMood_thenReturnSad() {
-		assertEquals("SAD",moodanalyze.analyseMood("I am in Sad mood"));
+		MoodAnalyzer moodanalyze= new MoodAnalyzer("I am in Sad mood");
+		assertEquals("SAD",moodanalyze.analyseMood());
 	}
 	@Test
 	public void givenMessage_whenInHappyMood_thenReturnHappy() {
-		assertEquals("HAPPY",moodanalyze.analyseMood("I am in Any mood"));
+		MoodAnalyzer moodanalyze= new MoodAnalyzer("I am in Any mood");
+		assertEquals("HAPPY",moodanalyze.analyseMood());
 	}
 
 }

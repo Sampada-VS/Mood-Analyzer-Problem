2214338bcc699e0e09f1e359d314e13e6031ff2e
Sampada-VS <samssvv1@gmail.com>
2020-10-30 03:17:17 +0530

    Adding- custom exception for handling exceptions

--- src/main/java/com/bridgelabz/moodanalyzer/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/moodanalyzer/MoodAnalyzer.java
@@ -8,14 +8,14 @@ public class MoodAnalyzer {
 		this.message=message;
 	}
 
-	public String analyseMood () {
+	public String analyseMood () throws MoodAnalysisException {
 		try {
 		if (message.contains("Sad"))
 			return "SAD";
 		else
 			return "HAPPY";
 		} catch (NullPointerException e) {
-			return "HAPPY";
+			throw new MoodAnalysisException("Please enter valid mood");
 		}
 
 	}

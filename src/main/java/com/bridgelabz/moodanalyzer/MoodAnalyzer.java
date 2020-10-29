5ae541ab2492b2e0008610995ebfc7ad0fa8be24
Sampada-VS <samssvv1@gmail.com>
2020-10-30 01:33:12 +0530

    Adding- function to return mood as per message

--- src/main/java/com/bridgelabz/moodanalyzer/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/moodanalyzer/MoodAnalyzer.java
@@ -2,8 +2,13 @@ package com.bridgelabz.moodanalyzer;
 
 public class MoodAnalyzer {
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to Mood analyzer problem.");
+	public static String analyseMood (String message) {
+		if (message.contains("sad"))
+			return "SAD mood";
+		else
+			return "HAPPY mmod";
 	}
-
+public static void main(String[] args) {
+	System.out.println(analyseMood("I am in mood"));
+}
 }
